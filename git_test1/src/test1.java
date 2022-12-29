374f4f77b9c945fb9485f983114a554cd6d6dadb
eye_chang <eye_chang@163.com>
2022-12-29 22:33:10 +0800

    IDEA_GIT4

--- git_test1/src/test1.java
+++ git_test1/src/test1.java
@@ -9,7 +9,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("hello IDEA GIT");
         System.out.println("hello IDEA GIT2");
-        System.out.println("hello IDEA GIT5");
+        System.out.println("hello IDEA GIT2");
         System.out.println("hello IDEA GIT4");
     }
 }

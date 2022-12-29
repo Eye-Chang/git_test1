a1a416505bf95fc444d36ac6b97aaf3e25e55bc6
eye_chang <eye_chang@163.com>
2022-12-29 21:56:01 +0800

    IDEA_GIT

---
+++ git_test1/src/test1.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName: PACKAGE_NAME.test1
+ * @author: lenovo
+ * @date: 2022/12/29 21:47
+ * @Description: 该类的功能描述
+ */
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("hello IDEA GIT");
+    }
+}
